6dfa3b6ec909a026e4108c900f582e941fadde70
KokiFujimoto <gegvp0330@gmail.com>
2024-03-13 01:33:18 +0900

    一旦ここ

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import vuetify from 'vite-plugin-vuetify';
+import vue from '@vitejs/plugin-vue2'
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
     server: {
@@ -14,6 +15,19 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
-        vuetify(),
+        Components({
+            resolvers: [VuetifyResolver()],
+        }),
     ],
+    define: { 'process.env': {} },
+    resolve: {
+        alias: [{ find: '@', replacement: '/src' }],
+    },
+    css: {
+        preprocessorOptions: {
+        scss: {
+            additionalData: "\n@import 'src/styles/variables.scss';\n",
+        },
+      },
+    },
 });
