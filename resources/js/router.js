4572db67b0cef5946f5097fd54d5e5c59895768d
KokiFujimoto <gegvp0330@gmail.com>
2024-03-21 15:53:37 +0900

    ルーティングの設定

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,13 +1,27 @@
+// router.js
+
 import { createRouter, createWebHistory } from "vue-router";
-import Hoge from "./components/Hoge.vue";
+import Home from "./pages/Home/index.vue";
+import Customer from "./pages/Customer/index.vue";
+import Chart from "./pages/Chart/index.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: "/hoge",
-            name: "hoge",
-            component: Hoge,
+            path: "/",
+            name: "home",
+            component: Home,
+        },
+        {
+            path: "/customer",
+            name: "customer",
+            component: Customer,
+        },
+        {
+            path: "/chart",
+            name: "chart",
+            component: Chart,
         },
     ],
 });
