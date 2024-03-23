20e339096ef70b8dfb3bcc4b7ad2b780ab2acc33
KokiFujimoto <gegvp0330@gmail.com>
2024-03-23 12:04:17 +0900

    migration model controllerの作成

---
+++ database/migrations/2024_03_23_030025_create_d_customers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("d_customers", function(Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("kana");
+            $table->integer("birthday")->default(19000101);
+            $table->integer("occupation_id")->default(0);
+            $table->String("address");
+            $table->String("tel");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop("d_customers");
+    }
+};
