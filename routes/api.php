4c658a5cb9f8c449dbc85a810ba17b879b8738f7
fujimoto <gegvp0330@gmail.com>
2024-03-28 02:22:44 +0900

    api.phpはコマンドで作成　コントローラの書き方に注意

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
-// Route::prefix("/customer")->group(function() {
-//     Route::get("/search", "CustomerController@search");
-// });
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::post('/customer/search', [CustomerController::class, 'search']);
+Route::post("/customer/search", [CustomerController::class, "search"]);
